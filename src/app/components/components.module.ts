9708e747ee5732c175fd25ad8445ad492c89bca4
Satya-Chachaundiya <72746438+Satya-Chachaundiya@users.noreply.github.com>
2021-01-16 18:10:04 +0530

    Assignment 2

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { UsersComponent } from "./users/users.component";
+import { HighlighterDirective } from './highlighter.directive';
 
 @NgModule({
-    declarations: [UsersComponent],
+    declarations: [UsersComponent, HighlighterDirective, ],
     imports: [CommonModule],
     exports: [UsersComponent],
 })
